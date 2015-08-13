3cf2a12adb14786a3733414283cc64b6f8e4fab0
Jason Lane & andrew dang <student@epicodus.com>
2015-08-13 09:50:44 -0700

    Add class method .all

--- lib/address.rb
+++ lib/address.rb
@@ -1,10 +1,15 @@
 class Address
   attr_reader(:zip, :street, :state, :city)
+  @@address_array = []
 
   def initialize(attributes)
     @zip = attributes[:zip]
     @state = attributes[:state]
     @city = attributes[:city]
     @street = attributes[:street]
+  end
+
+  def self.all
+    @@address_array
   end
 end
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -2,14 +2,23 @@ require('address')
 require('rspec')
 
 describe Address do
+  before do
+    @new_address = Address.new({city: 'Detroit', state: 'Michigan', zip: '48219', street: '16745 Edinborough'})
+  end
+
+
   describe '#initialize' do
     it 'returns mailing attributes' do
-      new_address = Address.new({city: 'Detroit', state: 'Michigan', zip: '48219', street: '16745 Edinborough'})
-      expect(new_address.city).to eq "Detroit"
-      expect(new_address.state).to eq "Michigan"
-      expect(new_address.zip).to eq "48219"
-      expect(new_address.street).to eq "16745 Edinborough"
+      expect(@new_address.city).to eq "Detroit"
+      expect(@new_address.state).to eq "Michigan"
+      expect(@new_address.zip).to eq "48219"
+      expect(@new_address.street).to eq "16745 Edinborough"
     end
+  end
 
+  describe '.all' do
+    it('initializes an emtpy array') do
+      expect(Address.all).to eq []
+    end
   end
 end
