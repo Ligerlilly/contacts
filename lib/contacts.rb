7e638d3fea900596a40dbdab0580d02115a63e86
Jason Lane & andrew dang <student@epicodus.com>
2015-08-13 09:30:33 -0700

    Add class method .all

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,10 +1,15 @@
 class Contact
   attr_reader(:first_name, :last_name, :job_title, :company)
-
+  @@contact_array = []
   def initialize(attributes)
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
     @job_title = attributes[:job_title]
     @company = attributes[:company]
   end
+
+  def self.all
+    @@contact_array
+  end
+
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -5,12 +5,19 @@ describe(Contact) do
   before do
     @new_contact = Contact.new({first_name: 'Jason', last_name: 'Lane', job_title: 'Student', company: 'Epicodus'})
   end
+
   describe('#initialize') do
     it('initializes attributes for contact class') do
       expect(@new_contact.first_name).to eq 'Jason'
       expect(@new_contact.last_name).to eq 'Lane'
       expect(@new_contact.job_title).to eq 'Student'
       expect(@new_contact.company).to eq 'Epicodus'
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array at first') do
+      expect(Contact.all()).to eq []
     end
   end
 end
